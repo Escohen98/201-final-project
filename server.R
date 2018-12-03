d8e8b09ab33d692fd627df3b5b41eb8debfdb1fa
Lucas Tucker <ltuck99@uw.edu>
2018-12-02 20:11:53 -0800

    added a chart to server

--- server.R
+++ server.R
@@ -33,5 +33,53 @@ server <- function(input, output) {
     home_and_away
   })
   
+  ## Creates a data frame of the last nine home games of the home team
+  home_team_data <- reactive({
+    temp <- home_and_away_teams()
+    homeTeam <- temp[2]
+    homeData <- filter(game_data, team_home == homeTeam)
+    homeData <- head(homeData, 9)
+    homeData
+  })
   
+  ## Creates a data frame of the last nine away gsmes of the away team
+  away_team_data <- reactive({
+    temp <- home_and_away_teams()
+    awayTeam <- tempA[1]
+    awayData <- filter(game_data, team_away == awayTeam)
+    awayData <- head(awayData, 9)
+    awayData
+  })
+  
+  ## Returns a chart displaying the home win rate of the home team and the away win rate of the away team
+  output$home_and_away_chart <- renderPlot({
+    homeData <- home_team_data()
+    awayData <- away_team_data()
+    team_names <- home_and_away_teams()
+    
+    ## Records the number of home and away wins of gthe home and away teams
+    homeWins <- 0
+    awayWins <- 0
+    for(i in 1:9) {
+      if(homeData[i, "score_home"] > homeData[i, "score_away"]) {
+        homeWins <- homeWins + 1 
+      }
+      if(awayData[i, "score_home"] < awayData[i, "score_away"]) {
+        awayWins <- awayWins + 1 
+      }
+    }
+    homeWinRate <- homeWins / 9
+    awayWinRate <- awayWins / 9
+    
+    ## Formats strings for the title
+    team_names_detailed <- c(paste(team_names[1], "in away games"),
+                             paste(team_names[2], "in home games"))
+    winRateChart <- data_frame("Team_Name" = team_names,
+                               "Win_Rate" = c(awayWinRate, homeWinRate))
+    
+    ## Chart that is being returned
+    ggplot(data = winRateChart, aes(x = Team_Name, y = Win_Rate)) + geom_bar() +
+      ggtitle(paste("Win Rates for", team_names_detailed[1],
+                    "and", team_names_detailed[2]))
+  })
 }
