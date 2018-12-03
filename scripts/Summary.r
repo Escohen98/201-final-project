b4ed09fe4fb1132f117fdbfaffdee4ec593f9a20
Eric Cohen <escohen@uw.edu>
2018-12-02 18:44:33 -0800

    Starting on data_mod as per request by Lucas.

--- scripts/Summary.r
+++ scripts/Summary.r
@@ -60,7 +60,3 @@ shorten_name <- function(full_name) {
   tail(strsplit(full_name, " ")[[1]], 1)
 }
 
-##Added Columns to CSV. 
-#something <- mutate(spreadspoke, home_id = name_to_id(shorten_name(team_home)), away_id = name_to_id(shorten_name(team_away)))
-#write.csv(something, file = "../data/spreadspoke_scores.csv", row.names=FALSE)
-
