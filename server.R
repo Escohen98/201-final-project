77ae256cb95013e607f59c9c78128f61da52c6b3
Lucas Tucker <ltuck99@uw.edu>
2018-12-02 20:01:26 -0800

    modified server

--- server.R
+++ server.R
@@ -1,18 +1,22 @@
 library(shiny)
 
 server <- function(input, output) {
+  ## Gets current date
   current_date <- strtoi(substr(date(), nchar(date()) - 3, nchar(date())))
+  
+  ## Data frames used for functions
   stadiums <- read.csv("../data/nfl_stadiums.csv", stringsAsFactors = FALSE)
   teams <- read.csv("../data/nfl_teams.csv", stringsAsFactors = FALSE)
-  spreadspoke <- read.csv("../data/spreadspoke_scores.csv", stringsAsFactors = FALSE)
   
+  ## Game data frame
   game_data <- read.csv("data/spreadspoke_scores.csv") %>% 
     filter(schedule_season == current_date | schedule_season == current_date - 1 |
              schedule_season == current_date - 2)
   game_data$schedule_week <- as.numeric(game_data$schedule_week)
   game_data <- arrange(game_data, desc(schedule_season), desc(schedule_week))
   
-  
+  ## Creats a vector where
+  ## first data point is the away team of the inputted game, second point is the home team
   home_and_away_teams <- reactive({
     week <- input$schedule_week
     gameTitle <- input$game_name
@@ -28,4 +32,6 @@ server <- function(input, output) {
     home_and_away <- c(awayTeam, homeTeam)
     home_and_away
   })
+  
+  
 }
