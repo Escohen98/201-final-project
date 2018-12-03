d1d7f376774819d598b7e8b3aa4a975c6577902f
Lucas Tucker <ltuck99@uw.edu>
2018-12-02 20:01:40 -0800

    modified server

--- scripts/serverB.R
+++ scripts/serverB.R
@@ -6,9 +6,10 @@ source("summary.R")
 server <- function(input, output) {
   
   current_date <- strtoi(substr(date(), nchar(date()) - 3, nchar(date())))
+  
   stadiums <- read.csv("../data/nfl_stadiums.csv", stringsAsFactors = FALSE)
   teams <- read.csv("../data/nfl_teams.csv", stringsAsFactors = FALSE)
-  spreadspoke <- read.csv("../data/spreadspoke_scores.csv", stringsAsFactors = FALSE)
+  
   game_data <- read.csv("../data/spreadspoke_scores.csv") %>% 
     filter(schedule_season == current_date | schedule_season == current_date - 1 |
              schedule_season == current_date - 2)
@@ -33,10 +34,45 @@ server <- function(input, output) {
   })
   
   home_team_data <- reactive({
-    tempA <- home_and_away_teams()
-    homeTeam <- tempA[2]
+    temp <- home_and_away_teams()
+    homeTeam <- temp[2]
     homeData <- filter(game_data, team_home == homeTeam)
     homeData <- head(homeData, 9)
+    homeData
+  })
+  
+  away_team_data <- reactive({
+    temp <- home_and_away_teams()
+    awayTeam <- tempA[1]
+    awayData <- filter(game_data, team_away == awayTeam)
+    awayData <- head(awayData, 9)
+    awayData
+  })
+  
+  output$home_and_away_chart <- renderPlot({
+    homeData <- home_team_data()
+    awayData <- away_team_data()
+    team_names <- home_and_away_teams()
+    homeWins <- 0
+    awayWins <- 0
+    for(i in 1:9) {
+      if(homeData[i, "score_home"] > homeData[i, "score_away"]) {
+        homeWins <- homeWins + 1 
+      }
+      if(awayData[i, "score_home"] < awayData[i, "score_away"]) {
+        awayWins <- awayWins + 1 
+      }
+    }
+    homeWinRate <- homeWins / 9
+    awayWinRate <- awayWins / 9
     
+    team_names[1] <- paste(team_names[1], "in away games")
+    team_names[2] <- paste(team_names[2], "in home games")
+    winRateChart <- data_frame("Team_Name" = team_names,
+                               "Win_Rate" = c(awayWinRate, homeWinRate))
+
+    
+    ggplot(data = winRateChart, aes(x = Team_Name, y = Win_Rate)) + geom_bar() +
+      ggtitle(paste("Win Rate for", ))
   })
 }
