7702516bf5433782d3bb1abeb25a0a72e6d301ed
Josh Manning <manning.josh1@gmail.com>
2018-12-03 11:52:40 -0800

    UI update and small server.R change (selected_week)

--- server.R
+++ server.R
@@ -2,7 +2,7 @@ library(shiny)
 
 server <- function(input, output) {
   
-  output$schedule <- renderPrint ({ input$schedule })
+  output$selected_week <- renderText ({ input$schedule })
   
   ## Gets current date
   current_date <- strtoi(substr(date(), nchar(date()) - 3, nchar(date())))
