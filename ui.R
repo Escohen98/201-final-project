ed8d37843082ce5b8046f200b244f5c9d3f01a68
Lucas Tucker <ltuck99@uw.edu>
2018-12-04 15:20:47 -0800

    organizing

--- ui.R
+++ ui.R
@@ -2,6 +2,7 @@ library(shiny)
 library(dplyr)
 
 current_date <- strtoi(substr(date(), nchar(date()) - 3, nchar(date())))
+buttons <- c("Not Very Important", "Fairly Important", "Very Important")
 
 game_data <- read.csv("data/spreadspoke_scores.csv") %>% 
   filter(schedule_season == current_date)
@@ -23,17 +24,32 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("game", label = h3("Game Select"),
-                  #choices = textOutput("selected_week"))
-
-                  choices = list_of_games)
+                  choices = list_of_games),
+      
+      radioButtons("win_rate_importance", label =
+                     ("How Important is Overall Win Rate of the Teams to You?"),
+                   choices = buttons, selected = "Very Important"),
+      
+      radioButtons("win_rate_home_away_importance", label =
+                     ("How Important is the Win Rate of the Teams Based on Home Field advantage to You?"),
+                   choices = buttons, selected = "Fairly Important"),
+      radioButtons("head_to_head_importance", label =
+                     ("How Important is the Outcome of Head-to-Head Games Between the Two Teams to You?"),
+                   choices = buttons, selected = "Fairly Important"),
+      radioButtons("point_differential_importance", label =
+                     ("How Important is the Point Differential of the Teams to You?"),
+                   choices = buttons, selected = "Very Important"),
+      radioButtons("weather_importance", label =
+                     ("How Important is the Play of Teams in Weather Similar to the Upcoming Game to You?"),
+                   choices = buttons, selected = "Fairly Important")
     ),
   
   mainPanel(
     tabsetPanel(type = "tabs",
-                tabPanel("About The Site", textOutput("nine_game_mention")),
-                tabPanel("Team Win Rates", plotOutput("home_versus_away_chart")),
-                tabPanel("Rates: Home vs Away", plotOutput("home_and_away_chart")),
-                tabPanel("Point Differential", plotOutput("point_differential_chart"))
+                tabPanel("Team Win Rates", plotlyOutput("home_versus_away_chart")),
+                tabPanel("Rates: Home vs Away", plotlyOutput("home_and_away_chart")),
+                tabPanel("Point Differential", plotOutput("point_differential_chart")),
+                tabPanel("About The Site", textOutput("nine_game_mention"))
     )
   )
   )
