1eef059ad4299b5ca3b97f942d9a97f0db364aeb
Escohen <escohen98@gmail.com>
2018-12-05 08:13:57 -0800

    Weather graph not working yet.

--- ui.R
+++ ui.R
@@ -29,30 +29,30 @@ shinyUI(fluidPage(
       #             choices = c("Above 50 Degrees", "Below 50 Degrees")),
       
       radioButtons("win_rate_importance", label =
-                     ("How Important is Overall Win Rate of the Teams to You?"),
+                     ("How Important is Overall Win Rate of the Teams?"),
                    choices = buttons, selected = "Very Important"),
       
       radioButtons("win_rate_home_away_importance", label =
-                     ("How Important is the Win Rate of the Teams Based on Home Field advantage to You?"),
+                     ("How Important is the Win Rate of the Teams Based on Home Field advantage?"),
                    choices = buttons, selected = "Fairly Important"),
       radioButtons("head_to_head_importance", label =
-                     ("How Important is the Outcome of Head-to-Head Games Between the Two Teams to You?"),
+                     ("How Important is the Outcome of Head-to-Head Games Between the Two Teams?"),
                    choices = buttons, selected = "Fairly Important"),
       radioButtons("point_differential_importance", label =
-                     ("How Important is the Point Differential of the Teams to You?"),
+                     ("How Important is the Point Differential of the Teams?"),
                    choices = buttons, selected = "Very Important")
       #radioButtons("weather_importance", label =
-      #               ("How Important is the Play of Teams in Weather Similar to the Upcoming Game to You?"),
+      #               ("How Important is the Play of Teams in Weather Similar to the Upcoming Game?"),
       #             choices = buttons, selected = "Fairly Important")
     ),
   
   mainPanel(
     h1("Welcome to the NFL Game Predictor!"),
-    h3("Data Collected from:", a("Here", href = 'https://www.kaggle.com/tobycrabtree/nfl-scores-and-betting-data')),
+    h3("Data Collected from:", a("Spreadspoke", href = 'https://www.kaggle.com/tobycrabtree/nfl-scores-and-betting-data')),
     tabsetPanel(type = "pills",
                 tabPanel("Prediction", h3(textOutput("winning_team")),
                          img("", src =
-                               "https://www.stmaryriverside.org/cms/lib/IL50000027/Centricity/Domain/80/JVfootball.png")),
+                               "https://www.stmaryriverside.org/cms/lib/IL50000027/Centricity/Domain/80/JVfootball.png", alt="Football")),
                 tabPanel("Team Win Rates", plotlyOutput("home_versus_away_chart"),
                          textOutput("win_rate_explanation")),
                 tabPanel("Rates: Home vs Away", plotlyOutput("home_and_away_chart"),
@@ -61,7 +61,8 @@ shinyUI(fluidPage(
                          textOutput("point_differential_description")),
                 tabPanel("Head-to-Head", plotlyOutput("head_to_head_plot"),
                          textOutput("head_to_head_description")),
-                #tabPanel("Weather", plotlyOutput("weather_chart")),
+                tabPanel("Weather", plotlyOutput("weather_chart")),
+                         textOutput("weather_chart_description"),
                 tabPanel("About The Site", h4(textOutput("about")),
                          h5((textOutput("nine_game_mention"))))
     )
