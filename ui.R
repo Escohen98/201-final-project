e5db59d575bda40a0ab22f65441a37fbb1546eb3
Lucas Tucker <ltuck99@uw.edu>
2018-12-04 18:06:51 -0800

    added chart

--- ui.R
+++ ui.R
@@ -51,6 +51,7 @@ shinyUI(fluidPage(
                 tabPanel("Team Win Rates", plotlyOutput("home_versus_away_chart")),
                 tabPanel("Rates: Home vs Away", plotlyOutput("home_and_away_chart")),
                 tabPanel("Point Differential", plotlyOutput("point_differential_chart")),
+                tabPanel("Head-to-Head", plotlyOutput("head_to_head_plot")),
                 tabPanel("About The Site", textOutput("nine_game_mention"))
     )
   )
