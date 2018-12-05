05fcbc46ef1de887ac30efd5367fe69663aa846c
Lucas Tucker <ltuck99@uw.edu>
2018-12-04 22:28:21 -0800

    Lucas's attempt at the weather

--- scripts/serverB.R
+++ scripts/serverB.R
@@ -46,20 +46,124 @@ server <- function(input, output) {
     home_and_away
   })
   
-  ## Describes the head-to-head chart
- output$head_to_head_description <- renderText({
-   paste("This plot compares the win rate of the two teams in head-to-head",
-         "matches (i.e. games where they played each other)",
-         "over the last three years")
- })
- 
- 
- ## Describes the point differential chart
- output$point_differential_description <- renderText({
-   paste("This plot compares the point differentials of the two teams.",
-         "Point differential is the total points scored by a team subtracted by",
-         "the total points they have allowed their opponents to score")
- })
+  home_team_weather <- reactive({
+    teamNames <- home_and_away_teams()
+    homeTeam <- teamNames[2]
+    data <- filter(game_data, score_home != "NA",
+                   schedule_week <= week | schedule_season < current_date,
+                   team_home == homeTeam | team_away == homeTeam,
+                   weather_temperature != "NA")
+    
+    numWarmGames <- 0
+    numColdGames <- 0
+    warmRecord <- 0
+    coldRecord <- 0
+    for (i in 1:nrow(data)) {
+      if (homeTeam == paste(data[i, "team_home"])) {
+        if (data[i, "weather_temperature"] >= 50) {
+          if (data[i, "score_home"] > data[i, "score_away"]) {
+            warmRecord <- warmRecord + 1
+          }
+          numWarmGames <- numWarmGames + 1
+        } else {
+          if (data[i, "score_home"] > data[i, "score_away"]) {
+            coldRecord <- coldRecord + 1
+          }
+          numColdGames <- numColdGames + 1
+        }
+      } else {
+        if (data[i, "weather_temperature"] >= 50) {
+          if (data[i, "score_home"] < data[i, "score_away"]) {
+            warmRecord <- warmRecord + 1
+          }
+          numWarmGames <- numWarmGames + 1
+        } else {
+          if (data[i, "score_home"] < data[i, "score_away"]) {
+            coldRecord <- coldRecord + 1
+          }
+          numColdGames <- numColdGames + 1 
+        }
+      }
+    }
+    
+    if (numWarmGames == 0)
+      numWarmGames <- 1
+    if (numColdGames == 0)
+      numColdGames <- 1
+    
+    warmRecord <- warmRecord / numWarmGames
+    coldRecord <- coldRecord / numColdGames
+    
+    home_team_weather <- data_frame("Team_Name" = c("Cold Weather", "Warm Weather"),
+                                    "Temp_Based_Record" = c(coldRecord, warmRecord))
+  })
   
+  away_team_weather <- reactive({
+    teamNames <- home_and_away_teams()
+    awayTeam <- teamNames[1]
+    data <- filter(game_data, score_home != "NA",
+                   schedule_week <= week | schedule_season < current_date,
+                   team_home == awayTeam | team_away == awayTeam,
+                   weather_temperature != "NA")
+    
+    numWarmGames <- 0
+    numColdGames <- 0
+    warmRecord <- 0
+    coldRecord <- 0
+    for (i in 1:nrow(data)) {
+      if (awayTeam == paste(data[i, "team_home"])) {
+        if (data[i, "weather_temperature"] >= 50) {
+          if (data[i, "score_home"] > data[i, "score_away"]) {
+            warmRecord <- warmRecord + 1
+          }
+          numWarmGames <- numWarmGames + 1
+        } else {
+          if (data[i, "score_home"] > data[i, "score_away"]) {
+            coldRecord <- coldRecord + 1
+          }
+          numColdGames <- numColdGames + 1
+        }
+      } else {
+        if (data[i, "weather_temperature"] >= 50) {
+          if (data[i, "score_home"] < data[i, "score_away"]) {
+            warmRecord <- warmRecord + 1
+          }
+          numWarmGames <- numWarmGames + 1
+        } else {
+          if (data[i, "score_home"] < data[i, "score_away"]) {
+            coldRecord <- coldRecord + 1
+          }
+          numColdGames <- numColdGames + 1 
+        }
+      }
+    }
+    
+    if (numWarmGames == 0)
+      numWarmGames <- 1
+    if (numColdGames == 0)
+      numColdGames <- 1
+    
+    warmRecord <- warmRecord / numWarmGames
+    coldRecord <- coldRecord / numColdGames
+    
+    away_team_weather <- data_frame("Team_Name" = c("Cold Weather", "Warm Weather"),
+                                    "Temp_Based_Record" = c(coldRecord, warmRecord))
+  })
+  
+  weather_chart <- reactive({
+    temperature <- input$temp
+    teamNames <- home_and_away_teams()
+    home_team_weather <- home_team_weather()
+    away_team_weather <- away_team_weather()
+    
+    data <- data_frame("Team_Name" = teamNames,
+                       "Win_Rate" = c(0, 0))
+    if (temperature == paste("Below 50 Degrees"))
+      data$Win_Rate <- c(away_team_weather[1, 2], home_team_weather[1, 2])
+    else
+      data$Win_Rate <- c(away_team_weather[2, 2], home_team_weather[2, 2])
+    
+    data
+  })
   
 }
